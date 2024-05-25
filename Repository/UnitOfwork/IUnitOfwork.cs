a3f9a640f4bdf8e9e463bf174e0a03368a68be6f
Huy Pham <huypt110402@gmail.com>
2024-05-26 01:36:19 +0700

    Add GenericRepo + UnitOfwork

---
+++ Repository/UnitOfwork/IUnitOfwork.cs
@@ -0,0 +1,24 @@
+﻿using Repository.GenericRepository;
+using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.UnitOfwork
+{
+    public interface IUnitOfwork : IDisposable
+    {
+        IGenericRepository<Role> RoleRepo { get; set; }
+        IGenericRepository<Token> TokenRepo { get; set; }
+        IGenericRepository<Book> BookRepo { get; set; }
+        IGenericRepository<Category> CategoryRepo { get; set; }
+        IGenericRepository<User> UserRepo { get; set; }
+        IGenericRepository<Order> OrderRepo { get; set; }
+        IGenericRepository<OrderDetail> OrderDetailRepo { get; set; }
+
+        void Save();
+    }
+}
