013ba2a0e49da910641bf00baf89661ab659d8f7
Huy Pham <huypt110402@gmail.com>
2024-05-26 01:56:05 +0700

    config DB

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repository.UnitOfwork;
+
+namespace BookStore.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IUnitOfwork unitOfwork;
+
+        public BookController(IUnitOfwork unitOfwork)
+        {
+            this.unitOfwork = unitOfwork;
+        }
+        [HttpGet]
+        public IActionResult GetBook()
+        {
+            return Ok(unitOfwork.BookRepo.Get());
+        }
+    }
+}
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,3 +1,5 @@
+using BookStore.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register your dependencies
+builder.Services.AddDatabase();
+builder.Services.addUnitOfWork();
 
 var app = builder.Build();
 
